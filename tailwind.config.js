8ba5a24c86bb6730250ba6ea634a1fd42c4a7ffb
Karla Šmuk <karla.smuk@gmail.com>
2024-08-22 15:35:42 +0200

    added colors and font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      display: ["Open Sans"],
+    },
+    extend: {
+      colors: {
+        black: "#212121",
+        white: "#FFFFFF",
+        "bg-color": "#F4F5F7",
+        "button-grey": "#E8E8E8",
+        "border-grey": "#B6B6B6",
+        "text-grey": "#7C7C7C",
+      },
+    },
   },
   plugins: [],
 };
