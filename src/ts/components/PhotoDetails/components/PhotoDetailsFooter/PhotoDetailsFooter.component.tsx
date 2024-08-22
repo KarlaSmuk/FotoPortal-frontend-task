0f3c384dafaaf90b5ed7b205eb1742b0c2543562
Karla Šmuk <karla.smuk@gmail.com>
2024-08-22 19:22:11 +0200

    added keyword to photo footer and fixed displaying gallery info

--- src/ts/components/PhotoDetails/components/PhotoDetailsFooter/PhotoDetailsFooter.component.tsx
+++ src/ts/components/PhotoDetails/components/PhotoDetailsFooter/PhotoDetailsFooter.component.tsx
@@ -1,6 +1,7 @@
 import { PhotoDetailsPageData } from "../../../../pages/PhotoDetailsPage/types/PhotoDetailsData.type";
-import PhotoTextContainer from "../PhotoText/PhotoTextContainer.component";
-import PhotoTextParagraph from "../PhotoText/PhotoTextParagraph.component";
+import InfoContainer from "../../../InfoContainer/InfoContainer.component";
+import InfoContainerContent from "../../../InfoContainer/InfoContainerContent.component";
+import KeywordContainer from "../../../KeywordContainer/KeywordContainer.component";
 
 type PhotoDetailsFooterProps = {
   photo: PhotoDetailsPageData;
@@ -10,20 +11,35 @@ export default function PhotoDetailsFooter({ photo }: PhotoDetailsFooterProps) {
   return (
     <div className="bg-white mt-[675px] h-[411px] w-full">
       <div className="flex justify-between ml-[65px] mt-[41px] w-[669px] h-[65px]">
-        <PhotoTextContainer title={"Galerija"}>
-          {photo.gallery}
-        </PhotoTextContainer>
-        <PhotoTextContainer title={"Autor"}>
-          <PhotoTextParagraph lineHeight="19.07px" paragraph={photo.author} />
-        </PhotoTextContainer>
-        <PhotoTextContainer title={"Agencija"}>
-          <PhotoTextParagraph lineHeight="19.07px" paragraph={photo.agency} />
-        </PhotoTextContainer>
+        <InfoContainer title={"Galerija"}>
+          <InfoContainerContent
+            lineHeight="19.07px"
+            paragraph={photo.gallery}
+          />
+        </InfoContainer>
+        <InfoContainer title={"Autor"}>
+          <InfoContainerContent lineHeight="19.07px" paragraph={photo.author} />
+        </InfoContainer>
+        <InfoContainer title={"Agencija"}>
+          <InfoContainerContent lineHeight="19.07px" paragraph={photo.agency} />
+        </InfoContainer>
       </div>
       <div className="ml-[65px] w-[827px] mt-[21px]">
-        <PhotoTextContainer title={"Opis"}>
-          <PhotoTextParagraph lineHeight="22px" paragraph={photo.description} />
-        </PhotoTextContainer>
+        <InfoContainer title={"Opis"}>
+          <InfoContainerContent
+            lineHeight="22px"
+            paragraph={photo.description}
+          />
+        </InfoContainer>
+      </div>
+      <div className="ml-[65px] w-[574px] mt-[23px]">
+        <InfoContainer title={"Ključne riječi"}>
+          <div className="flex flex-wrap gap-[10px] mt-[15px]">
+            {photo.keywords.map((keyword) => {
+              return <KeywordContainer keyword={keyword} />;
+            })}
+          </div>
+        </InfoContainer>
       </div>
     </div>
   );
