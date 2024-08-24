3583471c9be39f1a8feaf4c8e76f61988052396e
Karla Šmuk <karla.smuk@gmail.com>
2024-08-24 15:53:55 +0200

    remove redundant height

--- src/ts/components/PhotoDetails/components/PhotoPreviewContainer/PhotoPreviewContainer.component.tsx
+++ src/ts/components/PhotoDetails/components/PhotoPreviewContainer/PhotoPreviewContainer.component.tsx
@@ -6,7 +6,7 @@ export default function PhotoPreviewContainer({
   url,
 }: PhotoPreviewContainerProps) {
   return (
-    <div className="bg-bg-color max-h-[634px] max-w-[956px] flex justify-center">
+    <div className="bg-bg-color max-w-[956px] flex justify-center">
       <img
         src={url}
         className="mt-[42px] mb-[65px] h-[527px] w-[300px] sm:w-[448px] object-cover"
