b91220e8636930430de4f330ab0355557c977a54
Karla Šmuk <karla.smuk@gmail.com>
2024-08-26 18:48:50 +0200

    added screens, updated black color to also have default one

--- src/ts/components/InfoContainer/InfoContainer.component.tsx
+++ src/ts/components/InfoContainer/InfoContainer.component.tsx
@@ -7,7 +7,7 @@ type InfoContainerProps = {
 
 export default function InfoContainer({ title, children }: InfoContainerProps) {
   return (
-    <div className="tracking-[-0.4px] text-black">
+    <div className="text-black-lighter tracking-[-0.4px]">
       <div className="mb-[8px] text-xs font-bold uppercase leading-[16px]">
         {title}
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,27 @@ export default {
     },
     extend: {
       colors: {
-        black: "#212121",
+        black: {
+          lighter: "#212121",
+        },
         white: "#FFFFFF",
         "bg-color": "#F4F5F7",
         "button-grey": "#E8E8E8",
         "border-grey": "#B6B6B6",
         "text-grey": "#7C7C7C",
+      },
+      screens: {
+        mobile: "320px",
+        // => @media (min-width: 640px) { ... }
+
+        tablet: "900px",
+        // => @media (min-width: 640px) { ... }
+
+        laptop: "1024px",
+        // => @media (min-width: 1024px) { ... }
+
+        desktop: "1357px",
+        // => @media (min-width: 1357px) { ... }
       },
     },
   },
