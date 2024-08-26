1b70e891ea017342bfa922d66e7b1858bda20dc7
Karla Šmuk <karla.smuk@gmail.com>
2024-08-26 15:12:56 +0200

    added top and bottom padding to keyword container

--- src/ts/components/KeywordContainer/KeywordContainer.component.tsx
+++ src/ts/components/KeywordContainer/KeywordContainer.component.tsx
@@ -6,7 +6,7 @@ type KeywordContainerProps = {
 
 export default function KeywordContainer({ keyword }: KeywordContainerProps) {
   return (
-    <div className="flex h-7 items-center rounded-[50px] border border-border-grey px-[15px]">
+    <div className="flex h-7 w-[500px] items-center rounded-[50px] border border-border-grey px-[15px] pb-[5px] pt-[4px]">
       <InfoContainerContent paragraph={keyword} />
     </div>
   );
